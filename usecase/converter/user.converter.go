2d0c691c6df9613ab6044dd09e39426c0b7656f7
yuita-yoshihiko <soup998211@gmail.com>
2024-09-14 23:03:02 +0900

    コンバーターの作成

--- usecase/converter/user.converter.go
+++ usecase/converter/user.converter.go
@@ -1,1 +1,24 @@
 package converter
+
+import (
+	"graphql-api/domain/models"
+	"graphql-api/domain/models/graphql"
+)
+
+type UserConverter interface {
+	ConvertUserModelToGraphQLType(*models.User) (*graphql.UserDetail, error)
+}
+
+type userConverterImpl struct {
+}
+
+func NewUserConverter() UserConverter {
+	return &userConverterImpl{}
+}
+
+func (c *userConverterImpl) ConvertUserModelToGraphQLType(m *models.User) (*graphql.UserDetail, error) {
+	return &graphql.UserDetail{
+		ID:   m.ID,
+		Name: m.Name,
+	}, nil
+}
