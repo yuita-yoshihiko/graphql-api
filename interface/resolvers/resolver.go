d0fb7c6d815f69f4f076c516d79a2416517d2b71
yuita-yoshihiko <soup998211@gmail.com>
2024-09-14 23:04:11 +0900

    リゾルバーインターフェース

--- interface/resolvers/resolver.go
+++ interface/resolvers/resolver.go
@@ -1,7 +1,28 @@
 package resolvers
 
+import (
+	"graphql-api/infrastructure/db"
+	"graphql-api/interface/resolvers/database"
+	"graphql-api/usecase"
+	"graphql-api/usecase/converter"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	UserUsecase usecase.UserUsecase
+}
+
+func NewResolver() *Resolver {
+	dbAdministrator := db.NewDBAdministrator(db.DB)
+	userRepository := database.NewUserRepository(dbAdministrator)
+	userUsecase := usecase.NewUserUsecase(
+		userRepository,
+		converter.NewUserConverter(),
+	)
+	return &Resolver{
+		UserUsecase: userUsecase,
+	}
+}
