b13d0fc01d7231b74f22cadac37526d4f7238324
yuita-yoshihiko <soup998211@gmail.com>
2024-09-15 01:04:33 +0900

    サーバー設定諸々

---
+++ route/graphql.go
@@ -0,0 +1,28 @@
+package route
+
+import (
+	"graphql-api/infrastructure/graphql"
+	"graphql-api/interface/resolvers"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo/v4"
+)
+
+func DefineGraphQL(r *resolvers.Resolver) echo.HandlerFunc {
+	config := graphql.Config{
+		Resolvers: r,
+	}
+	handler := handler.NewDefaultServer(graphql.NewExecutableSchema(config))
+	return func(c echo.Context) error {
+		handler.ServeHTTP(c.Response().Writer, c.Request())
+		return nil
+	}
+}
+
+func PlaygroundHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		playground.Handler("GraphQL", "/query/").ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
--- server.go
+++ server.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"graphql-api/infrastructure/graphql"
+	"graphql-api/infrastructure/db"
 	"graphql-api/interface/resolvers"
+	"graphql-api/route"
 	"log"
-	"net/http"
-	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
 
-	srv := handler.NewDefaultServer(graphql.NewExecutableSchema(graphql.Config{Resolvers: &resolvers.Resolver{}}))
+	db.Init()
+	e := echo.New()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	e.GET("/", route.PlaygroundHandler())
+	g := e.Group("/query")
+
+	resolver := resolvers.NewResolver()
+
+	g.POST("/", route.DefineGraphQL(resolver))
+
+	log.Fatal(e.Start(":8080"))
 }
