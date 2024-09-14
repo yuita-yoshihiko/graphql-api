e304e6b3aa244c33efca357350c1dc502bfc3da5
yuita-yoshihiko <soup998211@gmail.com>
2024-09-14 22:11:53 +0900

    ユーザーデータ取得処理

--- interface/resolvers/database/user.go
+++ interface/resolvers/database/user.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"context"
+	"graphql-api/domain/models"
+	"graphql-api/infrastructure/db"
+	"graphql-api/usecase/repository"
+)
+
+type userRepositoryImpl struct {
+	db db.DBAdministrator
+}
+
+func NewUserRepository(db db.DBAdministrator) repository.UserRepository {
+	return &userRepositoryImpl{db: db}
+}
+
+func (r *userRepositoryImpl) Fetch(ctx context.Context, id int64) (*models.User, error) {
+	user, err := models.Users(
+		models.UserWhere.ID.EQ(id),
+	).One(ctx, r.db.GetDao(ctx))
+	return user, r.db.Error(err)
+}
