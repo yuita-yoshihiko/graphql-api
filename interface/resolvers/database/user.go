63e19c035b07609c638bad8d0dc8846005fad71c
yuita-yoshihiko <soup998211@gmail.com>
2024-09-15 21:18:31 +0900

    データベース処理

--- interface/resolvers/database/user.go
+++ interface/resolvers/database/user.go
@@ -5,6 +5,8 @@ import (
 	"graphql-api/domain/models"
 	"graphql-api/infrastructure/db"
 	"graphql-api/usecase/repository"
+
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 type userRepositoryImpl struct {
@@ -20,4 +22,8 @@ func (r *userRepositoryImpl) Fetch(ctx context.Context, id int64) (*models.User,
 		models.UserWhere.ID.EQ(id),
 	).One(ctx, r.db.GetDao(ctx))
 	return user, r.db.Error(err)
+}
+
+func (r *userRepositoryImpl) Create(ctx context.Context, m *models.User) error {
+	return r.db.Error(m.Insert(ctx, r.db.GetDao(ctx), boil.Infer()))
 }
