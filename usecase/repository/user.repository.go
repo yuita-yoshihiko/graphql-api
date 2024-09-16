f2e3142b10a939c1b472cf6760f551dfbe066cc4
yuita-yoshihiko <soup998211@gmail.com>
2024-09-16 16:57:15 +0900

    repository interface

--- usecase/repository/user.repository.go
+++ usecase/repository/user.repository.go
@@ -9,4 +9,5 @@ type UserRepository interface {
 	Fetch(context.Context, int64) (*models.User, error)
 	Create(context.Context, *models.User) error
 	Update(context.Context, *models.User, []string) error
+	Delete(context.Context, *models.User) error
 }
