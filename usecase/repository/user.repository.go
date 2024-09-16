cdfa24799cef3600850219ff06f7027c5e3fa209
yuita-yoshihiko <soup998211@gmail.com>
2024-09-16 16:39:03 +0900

    repository interface

--- usecase/repository/user.repository.go
+++ usecase/repository/user.repository.go
@@ -8,4 +8,5 @@ import (
 type UserRepository interface {
 	Fetch(context.Context, int64) (*models.User, error)
 	Create(context.Context, *models.User) error
+	Update(context.Context, *models.User, []string) error
 }
