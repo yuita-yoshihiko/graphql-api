780efa5eeec1f43ff5c81f16699aad6394193768
yuita-yoshihiko <soup998211@gmail.com>
2024-09-22 22:26:57 +0900

    converter

---
+++ usecase/converter/comment.converter.go
@@ -0,0 +1,66 @@
+package converter
+
+import (
+	"graphql-api/domain/models"
+	"graphql-api/domain/models/graphql"
+	"graphql-api/utils"
+)
+
+type CommentConverter interface {
+	ConvertCommentModelToGraphQLType(*models.Comment) (*graphql.CommentDetail, error)
+	ConvertCommentModelsToGraphQLTypes([]*models.Comment) ([]*graphql.CommentDetail, error)
+	ConvertCommentGraphQLTypeToModel(graphql.CreateCommentInput) (*models.Comment, error)
+	ConvertCommentGraphQLTypeToModelUpdate(graphql.UpdateCommentInput) (*models.Comment, error)
+	ConvertRawArgsToDBColumnNames(map[string]interface{}) ([]string, error)
+}
+
+type commentConverterImpl struct {
+}
+
+func NewCommentConverter() CommentConverter {
+	return &commentConverterImpl{}
+}
+
+func (c *commentConverterImpl) ConvertCommentModelToGraphQLType(m *models.Comment) (*graphql.CommentDetail, error) {
+	return &graphql.CommentDetail{
+		ID:   m.ID,
+		User: &graphql.UserDetail{
+			ID: m.UserID,
+		},
+		Post: &graphql.PostDetail{
+			ID: m.PostID,
+		},
+	}, nil
+}
+
+func (c *commentConverterImpl) ConvertCommentModelsToGraphQLTypes(ms []*models.Comment) ([]*graphql.CommentDetail, error) {
+	commentDetails := []*graphql.CommentDetail{}
+	for _, m := range ms {
+		commentDetail, err := c.ConvertCommentModelToGraphQLType(m)
+		if err != nil {
+			return nil, err
+		}
+		commentDetails = append(commentDetails, commentDetail)
+	}
+	return commentDetails, nil
+}
+
+func (c *commentConverterImpl) ConvertCommentGraphQLTypeToModel(input graphql.CreateCommentInput) (*models.Comment, error) {
+	return &models.Comment{
+		UserID: input.UserID,
+		PostID: input.PostID,
+		Content: input.Content,
+	}, nil
+}
+
+func (c *commentConverterImpl) ConvertCommentGraphQLTypeToModelUpdate(input graphql.UpdateCommentInput) (*models.Comment, error) {
+	return &models.Comment{
+		Content: input.Content,
+	}, nil
+}
+
+func (c commentConverterImpl) ConvertRawArgsToDBColumnNames(rawArgs map[string]interface{}) ([]string, error) {
+	i := graphql.CommentDetail{}
+	m := models.Comment{}
+	return utils.ConvertRawArgsToColumnNames(rawArgs, i, m)
+}
---
+++ usecase/converter/post.converter.go
@@ -0,0 +1,66 @@
+package converter
+
+import (
+	"graphql-api/domain/models"
+	"graphql-api/domain/models/graphql"
+	"graphql-api/utils"
+)
+
+type PostConverter interface {
+	ConvertPostModelToGraphQLType(*models.Post) (*graphql.PostDetail, error)
+	ConvertPostModelsToGraphQLTypes([]*models.Post) ([]*graphql.PostDetail, error)
+	ConvertPostGraphQLTypeToModel(graphql.CreatePostInput) (*models.Post, error)
+	ConvertPostGraphQLTypeToModelUpdate(graphql.UpdatePostInput) (*models.Post, error)
+	ConvertRawArgsToDBColumnNames(map[string]interface{}) ([]string, error)
+}
+
+type postConverterImpl struct {
+}
+
+func NewPostConverter() PostConverter {
+	return &postConverterImpl{}
+}
+
+func (c *postConverterImpl) ConvertPostModelToGraphQLType(m *models.Post) (*graphql.PostDetail, error) {
+	return &graphql.PostDetail{
+		ID:   m.ID,
+		User: &graphql.UserDetail{
+			ID: m.UserID,
+		},
+		Title:   m.Title,
+		Content: m.Content,
+	}, nil
+}
+
+func (c *postConverterImpl) ConvertPostModelsToGraphQLTypes(ms []*models.Post) ([]*graphql.PostDetail, error) {
+	postDetails := []*graphql.PostDetail{}
+	for _, m := range ms {
+		postDetail, err := c.ConvertPostModelToGraphQLType(m)
+		if err != nil {
+			return nil, err
+		}
+		postDetails = append(postDetails, postDetail)
+	}
+	return postDetails, nil
+}
+
+func (c *postConverterImpl) ConvertPostGraphQLTypeToModel(input graphql.CreatePostInput) (*models.Post, error) {
+	return &models.Post{
+		UserID: input.UserID,
+		Title:	input.Title,
+		Content: input.Content,
+	}, nil
+}
+
+func (c *postConverterImpl) ConvertPostGraphQLTypeToModelUpdate(input graphql.UpdatePostInput) (*models.Post, error) {
+	return &models.Post{
+		Title:  input.Title,
+		Content: input.Content,
+	}, nil
+}
+
+func (c postConverterImpl) ConvertRawArgsToDBColumnNames(rawArgs map[string]interface{}) ([]string, error) {
+	i := graphql.PostDetail{}
+	m := models.Post{}
+	return utils.ConvertRawArgsToColumnNames(rawArgs, i, m)
+}
