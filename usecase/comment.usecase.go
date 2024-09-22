277561ecb7e13d6b68a02ed2d0ed19ea1723ab06
yuita-yoshihiko <soup998211@gmail.com>
2024-09-22 22:27:54 +0900

    usecase

---
+++ usecase/comment.usecase.go
@@ -0,0 +1,73 @@
+package usecase
+
+import (
+	"context"
+	"graphql-api/domain/models/graphql"
+	"graphql-api/usecase/converter"
+	"graphql-api/usecase/repository"
+	"graphql-api/utils"
+)
+
+type CommentUsecase interface {
+	Fetch(context.Context, int64) (*graphql.CommentDetail, error)
+	FetchByPostID(context.Context, int64) ([]*graphql.CommentDetail, error)
+	Create(context.Context, graphql.CreateCommentInput) (*graphql.CommentDetail, error)
+	Update(context.Context, graphql.UpdateCommentInput) (*graphql.CommentDetail, error)
+}
+
+type commentUsecaseImpl struct {
+	repository repository.CommentRepository
+	converter	converter.CommentConverter
+}
+
+func NewCommentUsecase(r repository.CommentRepository,
+	c converter.CommentConverter,
+	) CommentUsecase {
+	return &commentUsecaseImpl{
+		repository: r,
+		converter: c,
+	}
+}
+
+func (u *commentUsecaseImpl) Fetch(ctx context.Context, id int64) (*graphql.CommentDetail, error) {
+	c, err := u.repository.Fetch(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return u.converter.ConvertCommentModelToGraphQLType(c)
+}
+
+func (u *commentUsecaseImpl) FetchByPostID(ctx context.Context, id int64) ([]*graphql.CommentDetail, error) {
+	cs, err := u.repository.FetchByPostID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return u.converter.ConvertCommentModelsToGraphQLTypes(cs)
+}
+
+func (u *commentUsecaseImpl) Create(ctx context.Context, input graphql.CreateCommentInput) (*graphql.CommentDetail, error) {
+	c, err := u.converter.ConvertCommentGraphQLTypeToModel(input)
+	if err != nil {
+		return nil, err
+	}
+	if err := u.repository.Create(ctx, c); err != nil {
+		return nil, err
+	}
+	return u.Fetch(ctx, c.ID)
+}
+
+func (u *commentUsecaseImpl) Update(ctx context.Context, input graphql.UpdateCommentInput) (*graphql.CommentDetail, error) {
+	columns, err := u.converter.ConvertRawArgsToDBColumnNames(utils.GetRawArgs(ctx))
+	if err != nil {
+		return nil, err
+	}
+	c, err := u.converter.ConvertCommentGraphQLTypeToModelUpdate(input)
+	if err != nil {
+		return nil, err
+	}
+	c.ID = input.ID
+	if err := u.repository.Update(ctx, c, columns); err != nil {
+		return nil, err
+	}
+	return u.Fetch(ctx, c.ID)
+}
---
+++ usecase/post.usecase.go
@@ -0,0 +1,73 @@
+package usecase
+
+import (
+	"context"
+	"graphql-api/domain/models/graphql"
+	"graphql-api/usecase/converter"
+	"graphql-api/usecase/repository"
+	"graphql-api/utils"
+)
+
+type PostUsecase interface {
+	Fetch(context.Context, int64) (*graphql.PostDetail, error)
+	FetchByUserID(context.Context, int64) ([]*graphql.PostDetail, error)
+	Create(context.Context, graphql.CreatePostInput) (*graphql.PostDetail, error)
+	Update(context.Context, graphql.UpdatePostInput) (*graphql.PostDetail, error)
+}
+
+type postUsecaseImpl struct {
+	repository repository.PostRepository
+	converter	converter.PostConverter
+}
+
+func NewPostUsecase(r repository.PostRepository,
+	c converter.PostConverter,
+	) PostUsecase {
+	return &postUsecaseImpl{
+		repository: r,
+		converter: c,
+	}
+}
+
+func (u *postUsecaseImpl) Fetch(ctx context.Context, id int64) (*graphql.PostDetail, error) {
+	p, err := u.repository.Fetch(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return u.converter.ConvertPostModelToGraphQLType(p)
+}
+
+func (u *postUsecaseImpl) FetchByUserID(ctx context.Context, userID int64) ([]*graphql.PostDetail, error) {
+	ps, err := u.repository.FetchByUserID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+	return u.converter.ConvertPostModelsToGraphQLTypes(ps)
+}
+
+func (u *postUsecaseImpl) Create(ctx context.Context, input graphql.CreatePostInput) (*graphql.PostDetail, error) {
+	p, err := u.converter.ConvertPostGraphQLTypeToModel(input)
+	if err != nil {
+		return nil, err
+	}
+	if err := u.repository.Create(ctx, p); err != nil {
+		return nil, err
+	}
+	return u.Fetch(ctx, p.ID)
+}
+
+func (u *postUsecaseImpl) Update(ctx context.Context, input graphql.UpdatePostInput) (*graphql.PostDetail, error) {
+	columns, err := u.converter.ConvertRawArgsToDBColumnNames(utils.GetRawArgs(ctx))
+	if err != nil {
+		return nil, err
+	}
+	p, err := u.converter.ConvertPostGraphQLTypeToModelUpdate(input)
+	if err != nil {
+		return nil, err
+	}
+	p.ID = input.ID
+	if err := u.repository.Update(ctx, p, columns); err != nil {
+		return nil, err
+	}
+	return u.Fetch(ctx, p.ID)
+}
