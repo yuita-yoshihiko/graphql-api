443f4e7093914b13de92e90859b1b8e296b41c39
yuita-yoshihiko <soup998211@gmail.com>
2024-09-29 15:52:49 +0900

    複数のデータを取得できる形にdataloaderのコードを修正

--- interface/database/dataloader/comment.dataloader.go
+++ interface/database/dataloader/comment.dataloader.go
@@ -2,21 +2,20 @@ package dataloader
 
 import (
 	"context"
-	"errors"
 	"time"
 
-	"github.com/graph-gophers/dataloader/v7"
 	"graphql-api/domain/models/graphql"
 	"graphql-api/usecase/converter"
 	"graphql-api/usecase/repository"
+
+	"github.com/graph-gophers/dataloader/v7"
 )
 
-type CommentDataLoaderKey struct{}
-
-var CommentDataloaderKey CommentDataLoaderKey = CommentDataLoaderKey{}
+var CDataloaderKey CommentDataloaderKey = CommentDataloaderKey{}
 
 type (
-	CommentDataloader     Dataloader[int64, *graphql.CommentDetail]
+	CommentDataloaderKey  struct{}
+	CommentDataloader     Dataloader[int64, []*graphql.CommentDetail]
 	commentDataloaderImpl struct {
 		converter  converter.CommentConverter
 		repository repository.CommentRepository
@@ -32,40 +31,30 @@ func NewCommentDataLoader(c converter.CommentConverter, r repository.CommentRepo
 	}
 }
 
-func (d *commentDataloaderImpl) NewInterface() dataloader.Interface[int64, *graphql.CommentDetail] {
-	var opt dataloader.Option[int64, *graphql.CommentDetail]
+func (d *commentDataloaderImpl) NewInterface() dataloader.Interface[int64, []*graphql.CommentDetail] {
+	var opt dataloader.Option[int64, []*graphql.CommentDetail]
 	if d.waitTime > 0 {
-		opt = dataloader.WithWait[int64, *graphql.CommentDetail](d.waitTime)
+		opt = dataloader.WithWait[int64, []*graphql.CommentDetail](d.waitTime)
 	}
 	return dataloader.NewBatchedLoader(d.BatchFunc(), opt)
 }
 
-func (d *commentDataloaderImpl) BatchFunc() dataloader.BatchFunc[int64, *graphql.CommentDetail] {
-	return func(ctx context.Context, keys []int64) []*dataloader.Result[*graphql.CommentDetail] {
-		comments, err := d.repository.FetchMany(ctx, keys)
+func (d *commentDataloaderImpl) BatchFunc() dataloader.BatchFunc[int64, []*graphql.CommentDetail] {
+	return func(ctx context.Context, keys []int64) []*dataloader.Result[[]*graphql.CommentDetail] {
+		comments, err := d.repository.FetchByPostIDs(ctx, keys)
 		if err != nil {
-			return MakeErrorResults[int64, *graphql.CommentDetail](keys, err)
+			return MakeErrorResults[int64, []*graphql.CommentDetail](keys, err)
 		}
 
-		if len(comments) == 0 {
-			var results []*dataloader.Result[*graphql.CommentDetail]
-			for _, key := range keys {
-				results = append(results, &dataloader.Result[*graphql.CommentDetail]{Data: &graphql.CommentDetail{ID: key}, Error: nil})
-			}
-			return results
-		}
-
-		commentMap := make(map[int64]*graphql.CommentDetail, len(comments))
+		commentMap := make(map[int64][]*graphql.CommentDetail, len(keys))
 		for _, comment := range comments {
-			commentMap[comment.ID], err = d.converter.ConvertCommentModelToGraphQLType(comment)
+			commentDetail, err := d.converter.ConvertCommentModelToGraphQLType(comment)
 			if err != nil {
-				return MakeErrorResults[int64, *graphql.CommentDetail](keys, err)
+				return MakeErrorResults[int64, []*graphql.CommentDetail](keys, err)
 			}
+			commentMap[comment.PostID] = append(commentMap[comment.PostID], commentDetail)
 		}
 
-		if len(keys) != len(commentMap) {
-			return MakeErrorResults[int64, *graphql.CommentDetail](keys, errors.New("an error occured, length defferent from keys and results"))
-		}
-		return SortResults(keys, commentMap, false)
+		return SortResults[int64, []*graphql.CommentDetail](keys, commentMap, true)
 	}
 }
