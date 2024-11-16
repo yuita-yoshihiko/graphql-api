5f8251ae0e73a38ba4468cc852e878fdb8dfd852
yuita-yoshihiko <soup998211@gmail.com>
2024-11-16 18:49:59 +0900

    fetchのテスト

--- usecase/user.usecase_test.go
+++ usecase/user.usecase_test.go
@@ -14,6 +14,60 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+func TestAddInterviewStatusMemoFetch(t *testing.T) {
+	t.Helper()
+	d := testhelper.LoadFixture(
+		"../",
+		"testdata/users/fixtures/1",
+	)
+	dbAdministrator := db.NewDBAdministrator(d)
+	uc := usecase.NewUserUsecase(
+		database.NewUserRepository(dbAdministrator),
+		converter.NewUserConverter(),
+	)
+	ctx := context.Background()
+
+	tests := []struct {
+		name    string
+		args    int64
+		want    *graphql.UserDetail
+		wantErr bool
+	}{
+		{
+			name: "正常に取得できる",
+			args: 1,
+			want: &graphql.UserDetail{
+				ID:   1,
+				Name: "テスト太郎",
+			},
+			wantErr: false,
+		},
+		{
+			name:    "存在しないID",
+			args:    100,
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := uc.Fetch(ctx, tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Error(err)
+			}
+			opt := cmp.Options{
+				cmpopts.IgnoreFields(
+					graphql.UserDetail{},
+					"ID",
+				),
+			}
+			if diff := cmp.Diff(tt.want, got, opt); diff != "" {
+				t.Errorf("diff(-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
 func TestAddInterviewStatusMemoCreate(t *testing.T) {
 	t.Helper()
 	d := testhelper.LoadFixture(
