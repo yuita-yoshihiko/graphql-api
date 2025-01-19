37b183d9a4cd25876b1104a32df737a5fc628ff7
yuita-yoshihiko <soup998211@gmail.com>
2025-01-19 21:03:56 +0900

    レスポンスのロギング時にも一意のIDが出力されるようにする

--- infrastructure/graphql/logger_handler.go
+++ infrastructure/graphql/logger_handler.go
@@ -2,17 +2,18 @@ package graphql
 
 import (
 	"context"
+	"graphql-api/constants"
 	"log/slog"
 
 	"github.com/99designs/gqlgen/graphql"
 )
 
 func LoggerHandler(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
-		response := next(ctx)
-		if response.Errors == nil {
-			slog.InfoContext(ctx, "GraphQL Operation", "OperationName", response.Data)
-		} else {
-			slog.InfoContext(ctx, "GraphQL Operation", "Error", response.Errors)
-		}
-		return response
+	response := next(ctx)
+	if response.Errors == nil {
+		slog.InfoContext(ctx, "GraphQL Operation", "OperationName", response.Data, "UUID", ctx.Value(constants.RequestKey))
+	} else {
+		slog.InfoContext(ctx, "GraphQL Operation", "Error", response.Errors, "UUID", ctx.Value(constants.RequestKey))
+	}
+	return response
 }
