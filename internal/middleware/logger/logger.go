32248f4fc7f5adffa37060a8630dafcd1e8a7e19
yuita-yoshihiko <soup998211@gmail.com>
2025-01-19 21:03:30 +0900

    リクエスト毎に一意のIDを設定し、ロギングするようにする

--- internal/middleware/logger/logger.go
+++ internal/middleware/logger/logger.go
@@ -1,9 +1,12 @@
 package logger
 
 import (
+	"context"
+	"graphql-api/constants"
 	"log/slog"
 	"os"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,8 +14,11 @@ import (
 func LoggerMiddleWare() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			ctx := context.WithValue(c.Request().Context(), constants.RequestKey, uuid.New().String())
+			c.SetRequest(c.Request().WithContext(ctx))
 			logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 			slog.SetDefault(logger)
+			slog.InfoContext(c.Request().Context(), "Request", "UUID", ctx.Value(constants.RequestKey))
 			return next(c)
 		}
 	}
