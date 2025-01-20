abcef46d77b0850593dc6b8603e227a7f9a78615
yuita-yoshihiko <soup998211@gmail.com>
2025-01-21 01:10:58 +0900

    operationNameの取り方わかったので修正

--- infrastructure/graphql/request_logger.go
+++ infrastructure/graphql/request_logger.go
@@ -4,23 +4,19 @@ import (
 	"context"
 	"graphql-api/constants"
 	"log/slog"
-	"regexp"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/google/uuid"
 )
 
-var re = regexp.MustCompile(`(?m)^\s*(?:query|mutation|subscription)?\s*{\s*(\w+)`)
-
 func RequestLoggerHandler(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
 	oc := graphql.GetOperationContext(ctx)
 	ctx = context.WithValue(ctx, constants.RequestKey, uuid.New().String())
-	matches := re.FindStringSubmatch(oc.RawQuery)
-	if len(matches) > 1 {
-		slog.InfoContext(ctx, "Request", "OperationName", matches[1], "UUID", ctx.Value(constants.RequestKey))
+	if oc.OperationName != "" {
+		slog.InfoContext(ctx, "Request", "OperationName", oc.OperationName, "UUID", ctx.Value(constants.RequestKey))
 	} else {
 		slog.InfoContext(ctx, "Request", "OperationName", "No match found", "UUID", ctx.Value(constants.RequestKey))
 	}
-	ctx = context.WithValue(ctx, constants.OperationNameKey, matches[1])
+	ctx = context.WithValue(ctx, constants.OperationNameKey, oc.OperationName)
 	return next(ctx)
 }
