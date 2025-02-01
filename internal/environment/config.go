8a81b4bb25d3cfe5db31e1b79ae6a5dc90db4dbf
yuita-yoshihiko <soup998211@gmail.com>
2025-02-01 22:06:48 +0900

    環境変数追加

--- internal/environment/config.go
+++ internal/environment/config.go
@@ -6,6 +6,7 @@ type (
 	Config struct {
 		Env   Env
 		DBEnv DBEnv
+		Skeleton Skeleton
 	}
 	Env struct {
 		IsLocal bool `envconfig:"ENV_IS_LOCAL" default:"false"`
@@ -14,5 +15,9 @@ type (
 		DatabaseURL      string `envconfig:"DATABASE_URL" required:"true"`
 		DatabasePassword string `envconfig:"DATABASE_PASSWORD" required:"true"`
 		SSLMode          string `envconfig:"DATABASE_SSL_MODE" required:"true"`
+	}
+
+	Skeleton struct {
+		Names string `envconfig:"SKELETON_NAMES" required:"true"`
 	}
 )
